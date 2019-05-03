900fc5f5adae2db8724d1e8dcfafb9f9785bd56e
vic-cw <vic-cw@users.noreply.github.com>
2019-05-03 18:02:14 +0200

    Fix bug when declaring an action as web enabled
    Previously, when setting the "web" parameter of an action to true,
    a bug would have as consequence that action would not be reachable
    over HTTP, but the update operation would not detect it.
    
    This commit fixes it so that an action can reliably be declared
    as web.

--- client/src/main/java/org/projectodd/openwhisk/ActionOptions.java
+++ client/src/main/java/org/projectodd/openwhisk/ActionOptions.java
@@ -43,7 +43,7 @@ public class ActionOptions {
 
     public ActionOptions web(final boolean webEnabled) {
         if (webEnabled) {
-            putAnnotation("web-export", "true");
+            putAnnotation("web-export", true);
         }
         return this;
     }
